d9b92ec077c5ad57e7c64c403cf7bc4135c7ec80
Vivek Jain <vivayush@gmail.com>
2024-07-29 08:43:26 +0530

    prabhu

--- delete_dummy_data.py
+++ delete_dummy_data.py
@@ -1,75 +1,84 @@
-import string
-import psycopg2
 
 
-#Connect to DB
-def db_connect():
-        conn = psycopg2.connect(database="defaultdb", 
-                                      user="avnadmin", 
-                                      password="",
-                                      host="vivek-tree-vivek-tree.e.aivencloud.com", 
-                                      port=15050)    
-        return conn
 
 #Function to delete dummy data
-def delete_dummy_data(entity_name):
-    conn = db_connect()
-    
+def delete_dummy_data(conn, entity_name):
+    cursor = conn.cursor()
+
     if(entity_name == 'sites'):
-        cursor = conn.cursor()
         try:
-            cursor.execute('DELETE FROM "14trees_old".sites s WHERE s.id IN (select s.id from "14trees_old".sites s where s.name_marathi like \'Test Random Site%\' order by s.created_at desc limit 10)')
+            cursor.execute('DELETE FROM "14trees_2".sites s WHERE s.id IN (select s.id from "14trees_2".sites s where s.name_marathi like \'Test Random Site%\' order by s.created_at desc limit 10)')
             print('Deleted dummy sites successfully')
-            connection.close()
-    
         except Exception as e:
             print(f"An error occured{e}")
-            connection.close() 
     
     elif (entity_name == 'plots'):
-          cursor = conn.cursor()
           try:
-              cursor.execute('DELETE FROM "14trees_old".plots p WHERE p.id IN (select p.id from "14trees_old".plots p where p.plot_id  like \'dummy_plot_id%\' order by p.created_at desc limit 100)')
+              cursor.execute('DELETE FROM "14trees_2".plots p WHERE p.id IN (select p.id from "14trees_2".plots p where p.plot_id  like \'dummy_plot_id%\' order by p.created_at desc limit 100)')
               print('Deleted Dummy Plots')
-              connection.close()
            
           except Exception as e:
               print(f"An error occured {e}")
-              connection.close()
     
     elif (entity_name == 'trees'):
-          cursor = conn.cursor()   
           try:
-              cursor.execute('DELETE FROM "14trees_old".trees t WHERE t.id IN (select t.id from "14trees_old".trees t where t.planted_by  like \'DataAutomation\' order by t.created_at desc limit 1000)')
+              cursor.execute('DELETE FROM "14trees_2".trees t WHERE t.planted_by =  \'DataAutomation\'')
               print("Deleted dummy trees ")
-              connection.close()
           except Exception as e:
               print(f"An error occured {e}")
-              connection.close()
               
     elif (entity_name == 'users'):
-          cursor = conn.cursor()
           try:
-              cursor.execute('DELETE FROM "14trees_old".users where u.user_id like \'dummy%\' order by u.created_at desc') 
+              cursor.execute('DELETE FROM "14trees_2".users u where u.user_id like \'dummy%\'') 
               print("Deleted dummy users")
-              connection.close()
           except Exception as e:
               print(f"An error occured {e}")
-              connection.close()
               
     elif (entity_name == 'visits'):
-          cursor= conn.cursor()
           try:
-              cursor.execute('DELETE from "14trees_old".visits v where v.visit_name like \'dummy-visit%\' ')   
+              cursor.execute('DELETE from "14trees_2".visits v where v.visit_name like \'Dummy%\' ')   
               print('Deleted dummy visits')
-              connection.close()
           
           except Exception as e:
              print(f'An error occured {e}')
-             connection.close()
+
+    elif (entity_name == 'organisations'):
+          try:
+              cursor.execute('delete from "14trees_2"."groups" g where  g."name" like \'Dummy%\' ')   
+              print('Deleted dummy organisations')
+          
+          except Exception as e:
+             print(f'An error occured {e}')
+
+
+    elif (entity_name == 'donations'):
+          try:
+              cursor.execute('delete from "14trees_2".donations d where  d."Name" like \'dummy%\'  ')   
+              print('Deleted dummy donations')
+          
+          except Exception as e:
+             print(f'An error occured {e}')
+
+    elif (entity_name == 'visit_users'):
+          try:
+              cursor.execute('Delete FROM "14trees_2".visit_users v WHERE v.visit_id in (select v.id from "14trees_2".visits v where v.visit_name like \'Dummy%\')')   
+              print('Deleted dummy visit users')
+          
+          except Exception as e:
+             print(f'An error occured {e}')
+
+    elif (entity_name == 'org_users'):
+          try:
+              cursor.execute('Delete FROM "14trees_2".user_groups ug WHERE ug.group_id in (select g.id from "14trees_2"."groups" g where  g."name" like \'Dummy%\' )')   
+              print('Deleted dummy org users ')
+          
+          except Exception as e:
+             print(f'An error occured {e}')
              
     else:
          print('Provide a valid entity name')
          return         
+    
+    conn.commit()
          
 
--- dummy_data_generator.py
+++ dummy_data_generator.py
@@ -3,47 +3,97 @@ from faker import Faker
 import json
 import string
 import random
+import numpy as np
 import psycopg2
 import random
 from datetime import datetime
 import os
 from delete_dummy_data import delete_dummy_data
+import subprocess
 
 fake = Faker('en_IN')
+db_url = 'vivek-tree-vivek-tree.e.aivencloud.com'
+db_port = '15050'
+db_user = 'avnadmin'
+db_password = os.environ['DB_PASSWORD']
+db_name = 'defaultdb'
+db_schema = '14trees_2'
+
+#Connect to DB
+def get_db_connection():
+        conn = psycopg2.connect(database=db_name, 
+                                      user=db_user, 
+                                      password=db_password,
+                                      host=db_url, 
+                                      port=db_port)    
+        return conn
 
 
 #Function to connect to server and get site_ids
-def get_site_ids():
-    connection = psycopg2.connect(database="defaultdb", user="avnadmin", password=os.environ['DB_PASSWORD'], host="vivek-tree-vivek-tree.e.aivencloud.com", port=15050)    
+def get_dummy_site_ids_from_db():
+    connection = get_db_connection()
     cursor = connection.cursor()
     
-    cursor.execute('select s.id from "14trees_old".sites s where s.name_marathi like \'Test Random Site%\' order by s.created_at desc limit 10')
+    cursor.execute('select s.id from "14trees_2".sites s where s.name_marathi like \'Test Random Site%\' order by s.created_at desc limit 10')
     site_ids =  [row[0] for row in cursor.fetchall()]
    
     connection.close()
     return site_ids
 
 #Function to connect to server and get plot_ids
-def get_plot_ids():
-    connection = psycopg2.connect(database="defaultdb", user="avnadmin", password=os.environ['DB_PASSWORD'], host="vivek-tree-vivek-tree.e.aivencloud.com", port=15050)    
+def get_dummy_plot_ids_from_db():
+    connection = get_db_connection()
     cursor = connection.cursor()
     
-    cursor.execute('select p.id from "14trees_old".plots p where p.plot_id  like \'dummy_plot_id%\' order by p.created_at desc limit 100 ')
+    cursor.execute('select p.id from "14trees_2".plots p where p.plot_id  like \'dummy_plot_id%\' order by p.created_at desc limit 100 ')
     plot_ids =  [row[0] for row in cursor.fetchall()]
     
     connection.close()
     return plot_ids
 
 #Function to connect to server and get plant_type_ids
-def get_plant_type_ids():
-    connection = psycopg2.connect(database="defaultdb", user="avnadmin", password=os.environ['DB_PASSWORD'], host="vivek-tree-vivek-tree.e.aivencloud.com", port=15050)    
+def get_dummy_plant_type_ids_from_db():
+    connection = get_db_connection()
     cursor = connection.cursor()
     
-    cursor.execute('select distinct pt.id  from "14trees_old".plant_types pt limit 10')
+    cursor.execute('select distinct pt.id  from "14trees_2".plant_types pt limit 10')
     plant_type_ids =  [row[0] for row in cursor.fetchall()]
     print(plant_type_ids)
     connection.close()
     return plant_type_ids
+
+#Function to connect to server and get user_ids
+def get_dummy_user_ids_from_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    
+    cursor.execute('select u.id  from "14trees_2".users u where u.user_id like \'dummy%\' ORDER BY u.created_at DESC limit 1000')
+    user_ids =  [row[0] for row in cursor.fetchall()]
+    print(user_ids)
+    connection.close()
+    return user_ids
+
+#Function to connect to server and get visit_ids
+def get_dummy_visit_ids_from_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    
+    cursor.execute('select v.id  from "14trees_2".visits v where v.visit_name like \'Dummy%\' ORDER BY v.id DESC limit 1000')
+    visit_ids =  [row[0] for row in cursor.fetchall()]
+    print(visit_ids)
+    connection.close()
+    return visit_ids
+
+#Function to connect to server and get org_ids
+def get_dummy_org_ids_from_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    
+    cursor.execute('select g.id  from "14trees_2"."groups" g where  g."name" like \'Dummy%\' ORDER BY g.created_at DESC limit 1000')
+    group_ids =  [row[0] for row in cursor.fetchall()]
+    print(group_ids)
+    connection.close()
+    return group_ids
 
 
 #Function to generate dummy users
@@ -147,7 +197,7 @@ def generate_dummy_sites(num_records):
 #Function to generate dummy plots
 def generate_dummy_plots(num_records):
     plots = []
-    site_ids = get_site_ids()
+    site_ids = get_dummy_site_ids_from_db()
    
     for each_site_id in site_ids: 
         for i in range(num_records):
@@ -156,8 +206,8 @@ def generate_dummy_plots(num_records):
                 "name": "Test Random plot " + str(i+1),
                 "plot_id":"dummy-plot-id-"+ str(each_site_id)+"-"+str(fake.random_int(min=1 , max=2500)) ,
                 "tags": None,
-                "boundaries": {"type": "Polygon", "coordinates": [[[18.92883906964203, 73.7769217462353], [18.92705962338517, 73.77601906599243], [18.92691470408016, 73.77663242954684], [18.92764441915284, 73.77778245391168], [18.92883906964203, 73.7769217462353]]]},
-                "center": {"type": "Point", "coordinates": [18.92883906964203, 73.7769217462353]},
+                "boundaries": '{"type": "Polygon", "coordinates": [[[18.92883906964203, 73.7769217462353], [18.92705962338517, 73.77601906599243], [18.92691470408016, 73.77663242954684], [18.92764441915284, 73.77778245391168], [18.92883906964203, 73.7769217462353]]]}',
+                "center": '{"type": "Point", "coordinates": [18.92883906964203, 73.7769217462353]}',
                 "gat": None,
                 "status": None,
                 "land_type_mongo_id": None,
@@ -177,33 +227,33 @@ def generate_dummy_plots(num_records):
 # Function to generate dummy trees
 def generate_dummy_trees(num_records):
     trees = [] 
-    plot_ids = get_plot_ids()
-    plant_type_ids = get_plant_type_ids()
+    plot_ids = get_dummy_plot_ids_from_db()
+    plant_type_ids = get_dummy_plant_type_ids_from_db()
     
-    for each_plot_id in plot_ids:
+    for ind, each_plot_id  in enumerate(plot_ids):
         for i in range(num_records):
             record = {
                 
-                "sapling_id": str(970000 + i + 1), 
+                "sapling_id": str(960000 + ind*num_records+ i + 1), 
                 "plant_type_id": int(random.choice(plant_type_ids)),
                 "plot_id": each_plot_id,
-                "images": None,
+                # "images": None,
                 "tags": None,
                 "location": {},
                 "planted_by": 'DataAutomation',
-                "mapped_to_user": 3,
-                "mapped_to_group": 1,
+                "mapped_to_user": None,
+                "mapped_to_group": None,
                 "mapped_at": None,
-                "sponsored_by_user": 3,
-                "sponsored_by_group": 1,
-                "gifted_by": 3,
-                "gifted_to": 3,
+                "sponsored_by_user": None,
+                "sponsored_by_group": None,
+                "gifted_by": None,
+                "gifted_to": None,
                 "assigned_at": None,
                 "assigned_to": None,
-                "user_tree_images": None,
+                # "user_tree_images": None,
                 "mongo_id": None,
                 "tree_type": None,
-                "plot": None,
+                "plot": each_plot_id,
                 "description": None,
                 "event_id": None,
                 "memory_images": None,
@@ -221,19 +271,38 @@ def generate_dummy_trees(num_records):
 #Function to generate dummy visits
 def generate_dummy_visits(num_records):
     visits = []
-    site_ids = get_site_ids()
+    site_ids = get_dummy_site_ids_from_db()
     for i in range(num_records):
             record = {
                 
-                "visit_name": "dummy-visit-"+str(i+1),
+                "visit_name": "Dummy Visit-"+str(i+1),
                 "visit_date": datetime.now().strftime('%D'),
                 "site_id": random.choice(site_ids),
-                "visit_type": fake.random_element(elements=['FAMILY' , 'CORPORATE'])
-                            
+                "visit_type": fake.random_element(elements=['family' , 'corporate']),
+                "created_at": datetime.now().strftime('%D %H:%M:%S'),
+                "updated_at": datetime.now().strftime('%D %H:%M:%S')
             }
             visits.append(record)
     return visits
 
+
+#Function to generate dummy visit users
+def generate_dummy_visits_users(visit_ids, user_ids):
+    visit_users_records = []
+    print("User Ids passed: ", user_ids)
+    for visit_id in visit_ids:
+        user_count = random.randint(0, len(user_ids))
+        visit_users = np.random.choice(user_ids, size=user_count, replace=False)
+        print("Selected user ids for visit id: "+str(visit_id)+" are ", visit_users)
+
+        for user_id in visit_users:
+            record = { 
+                "visit_id": visit_id,
+                "user_id": int(user_id),
+                "created_at": datetime.now().strftime('%D %H:%M:%S')
+            }
+            visit_users_records.append(record)
+    return visit_users_records
 
 #Function to generate dummy organisations
 def generate_dummy_organisations(num_records):
@@ -242,15 +311,33 @@ def generate_dummy_organisations(num_records):
     for i in range(num_records):
             record = {
                 
-                "name": "dummy-group-"+str(i+1),
+                "name": "Dummy Organisation-"+str(i+1),
                 "type": "corporate",
                 "description": None,
                 "created_at": datetime.now().strftime('%D %H:%M:%S'),
-                "updated_at": None
+                "updated_at": datetime.now().strftime('%D %H:%M:%S')
                             
             }
             oraganisations.append(record)
     return oraganisations
+
+#Function to generate dummy org users
+def generate_dummy_org_users(org_ids, user_ids):
+    org_users_records = []
+    print("User Ids passed: ", user_ids)
+    for org_id in org_ids:
+        user_count = random.randint(0, len(user_ids))
+        org_users = np.random.choice(user_ids, size=user_count, replace=False)
+        print("Selected user ids for org id: "+str(org_id)+" are ", org_users)
+
+        for user_id in org_users:
+            record = { 
+                "user_id": int(user_id),
+                "group_id": org_id,
+                "created_at": datetime.now().strftime('%D %H:%M:%S')
+            }
+            org_users_records.append(record)
+    return org_users_records
 
 # Function to generate dummy donations
 def generate_dummy_donations(num_records):
@@ -318,57 +405,102 @@ def convert_to_csv(records, file_name):
     df.to_csv(file_name, index=False,encoding='utf-8-sig')
     print(f'Input data saved to {file_name}')
 
-def main(): 
-    
-    num_records = 10
+def upload_csv_to_db(csv_file, db_url, db_port, dbuser, db_password, db_schema, table_name): 
+    commands = '''
+        export pg_str="postgresql://{dbuser}:{db_password}@{db_url}:{db_port}/defaultdb?sslmode=require"; 
+        csvsql --db $pg_str --no-create --insert {csv_file} --db-schema "{db_schema}" --tables {table_name}
+    '''.format(csv_file=csv_file, db_url=db_url, db_port=db_port, dbuser=dbuser, db_password=db_password, db_schema=db_schema, table_name=table_name)
 
+    process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    out, err = process.communicate(commands)
+    print("Output: ", out)
+    print("Error: ", err)
+
+def create_data():
+    num_records = 10
    
     # dummy_sites = generate_dummy_sites(num_records)
     # print(f'Dummy Sites Json: {json.dumps(dummy_sites, ensure_ascii=False, indent=4)}')
-    # convert_to_csv(dummy_sites, 'dummy_sites.csv')
-
-    # export table_name=sites
-    # export file_name=dummy_sites.csv
-    # csvsql --db ${pg_str} --no-create --insert ${file_name} --db-schema "14trees_old" --tables ${table_name}
+    # csv_file = 'dummy_sites.csv'
+    # table_name = 'sites'
+    # convert_to_csv(dummy_sites, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
 
     
     # dummy_plots = generate_dummy_plots(num_records)
     # print(f'Dummy Plots Json: {json.dumps(dummy_plots, ensure_ascii=False, indent=4)}')
-    # convert_to_csv(dummy_plots, 'dummy_plots.csv')
+    # csv_file = 'dummy_plots.csv'
+    # table_name = 'plots'
+    # convert_to_csv(dummy_plots, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
 
-    # export table_name=plots
-    # export file_name=dummy_plots.csv
-    # csvsql --db ${pg_str} --no-create --insert ${file_name} --db-schema "14trees_old" --tables ${table_name}
 
     # dummy_users = generate_dummy_users(num_records)
-    # # print(f'Dummy Users Json: {json.dumps(dummy_users)}')
-    # convert_to_csv(dummy_users, 'dummy_users.csv')
-
-    # export table_name=users
-    # export file_name=dummy_users.csv
-    # csvsql --db ${pg_str} --no-create --insert ${file_name} --db-schema "14trees_old" --tables ${table_name}
+    # print(f'Dummy Users Json: {json.dumps(dummy_users, ensure_ascii=False, indent=4)}')
+    # csv_file = 'dummy_users.csv'
+    # table_name = 'users'
+    # convert_to_csv(dummy_users, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
 
     # dummy_visits = generate_dummy_visits(num_records)
-    # # print(f'Dummy Users Json: {json.dumps(dummy_users)}')
-    # convert_to_csv(dummy_visits, 'dummy_visits.csv')
+    # print(f'Dummy Visits Json: {json.dumps(dummy_visits, ensure_ascii=False, indent=4)}')
+    # csv_file = 'dummy_visits.csv'
+    # table_name = 'visits'
+    # convert_to_csv(dummy_visits, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
     
+    # dummy_visits_users = generate_dummy_visits_users(get_dummy_visit_ids_from_db(), get_dummy_user_ids_from_db())
+    # print(f'Dummy Visit-Users Json: {json.dumps(dummy_visits_users, ensure_ascii=False, indent=4)}')
+    # csv_file = 'dummy_visit_users.csv'
+    # table_name = 'visit_users'
+    # convert_to_csv(dummy_visits_users, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
+    
+
     # dummy_trees = generate_dummy_trees(num_records)
-    # # print('dummy trees data' ,dummy_trees)
-    # convert_to_csv(dummy_trees, 'dummy_trees-1.csv')
+    # print(f'Dummy Trees Json: {json.dumps(dummy_trees, ensure_ascii=False, indent=4)}')
+    # csv_file = 'dummy_trees.csv'
+    # table_name = 'trees'
+    # convert_to_csv(dummy_trees, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
     
-    # dummy_oraganisations = generate_dummy_organisations(num_records)
-    # # print(f'Dummy Users Json: {json.dumps(dummy_users)}')
-    # convert_to_csv(dummy_oraganisations, 'dummy_oraganisations.csv')
+    dummy_organisations = generate_dummy_organisations(num_records)
+    print(f'Dummy Organisations Json: {json.dumps(dummy_organisations, ensure_ascii=False, indent=4)}')
+    csv_file = 'dummy_organisations.csv'
+    table_name = 'groups'
+    convert_to_csv(dummy_organisations, csv_file)
+    upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
+
+    dummy_org_users = generate_dummy_org_users(get_dummy_org_ids_from_db(), get_dummy_user_ids_from_db())
+    print(f'Dummy Org-Users Json: {json.dumps(dummy_org_users, ensure_ascii=False, indent=4)}')
+    csv_file = 'dummy_org_users.csv'
+    table_name = 'user_groups'
+    convert_to_csv(dummy_org_users, csv_file)
+    upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
     
     # dummy_donations = generate_dummy_donations(num_records)
-    # # print(f'Dummy Users Json: {json.dumps(dummy_users)}')
-    # convert_to_csv(dummy_donations, 'dummy_donations.csv')
+    # print(f'Dummy Donations Json: {json.dumps(dummy_donations, ensure_ascii=False, indent=4)}')
+    # csv_file = 'dummy_donations.csv'
+    # table_name = 'donations'
+    # convert_to_csv(dummy_donations, csv_file)
+    # upload_csv_to_db(csv_file, db_url, db_port, db_user, db_password,db_schema, table_name)
 
-    #delete_dummy_data()
+def delete_data():
+    conn = get_db_connection()
+    delete_dummy_data(conn, 'trees')
+    delete_dummy_data(conn, 'plots')
+    delete_dummy_data(conn, 'visit_users')
+    delete_dummy_data(conn, 'visits')
+    delete_dummy_data(conn,'sites')
+    delete_dummy_data(conn, 'org_users')
+    delete_dummy_data(conn, 'users')
+    # delete_dummy_data(conn, 'donations')
+    delete_dummy_data(conn, 'organisations')
+    conn.close()
 
+def main(): 
+    delete_data()
+    # create_data()
+    # get_dummy_user_ids_from_db()
     
-
-
-
-
 main()
